7834dcade879830d4ec33a48eb853ea770cc7f89
arwa98 <arwa.ahmed@programsoffice.org>
2024-10-06 17:16:03 +0300

    Rename envs

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+alias k=`kubectl`
+# Loop through each directory in the current directory
+for dir in */; do
+  # Enter the directory
+  cd "$dir"
+  
+  # Run the kubectl delete and apply commands
+  echo "Running commands in $dir"
+  kubectl delete -f deployment.yml
+  kubectl apply -f .
+  
+  # Go back to the original directory
+  cd ..
+done
