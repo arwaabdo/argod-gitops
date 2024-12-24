fb72614821a2cadd70d6b144f85fae12a973de29
arwa98 <arwa.ahmed@programsoffice.org>
2024-12-24 22:13:10 +0200

    remove the taints

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 alias k=`kubectl`
+cd staging
 # Loop through each directory in the current directory
 for dir in */; do
   # Enter the directory
@@ -7,7 +8,7 @@ for dir in */; do
   
   # Run the kubectl delete and apply commands
   echo "Running commands in $dir"
-  kubectl delete -f deployment.yml
+  # kubectl delete -f deployment.yml
   kubectl apply -f .
   
   # Go back to the original directory
